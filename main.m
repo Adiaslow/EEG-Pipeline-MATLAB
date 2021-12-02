e6fe18fea918d9c68ac727e6347033e701758ae1
Adam Murray <a.murray0413@gmail.com>
2021-12-02 14:03:00 -0800

    update

--- main.m
+++ main.m
@@ -1,9 +1,10 @@
 eeglab
 
-filePath = "C:\Users\admin\Desktop\GitHub Repos\EEG-Pipeline-MATLAB"; % go to folder where data file is, hold shift, right click, click "Copy as path", paste it after the '='
-fileName = 'MUAD06022021EOFull';
+filePath = "C:\Users\admin\Desktop\GitHub Repos\EEG-Pipeline-MATLAB";
+% go to folder where data file is, hold shift, right click, click "Copy as path", paste it after the '='
+fileName = 'MUAD06022021EOFull2';
 fileType = '.xdf';
-fullPath = strcat(filePath, "\", fileName, fileType);
+fullPath = strcat(filePath, filesep, fileName, fileType);
 mkdir figures
 
 % load data
@@ -16,30 +17,20 @@ elseif strcmp(fileType, '.edf')
 end
 
 % set channel locations
-EEG = pop_chanedit(EEG, 'lookup','C:\\eeglab2021.1\\plugins\\dipfit4.3\\standard_BEM\\elec\\standard_1005.elc','changefield',{1 'labels' 'FP1'},'changefield',{2 'labels' 'FP2'},'changefield',{3 'labels' 'F3'},'changefield',{4 'labels' 'F4'},'changefield',{5 'labels' 'C3'},'changefield',{6 'labels' 'C4'},'changefield',{7 'labels' 'P3'},'changefield',{8 'labels' 'P4'},'changefield',{9 'labels' 'O1'},'changefield',{10 'labels' 'O2'},'changefield',{11 'labels' 'F7'},'changefield',{12 'labels' 'F8'},'changefield',{13 'labels' 'T3'},'changefield',{14 'labels' 'T4'},'changefield',{15 'labels' 'T5'},'changefield',{16 'labels' 'T6'},'changefield',{17 'labels' 'Fz'},'changefield',{18 'labels' 'Cz'},'changefield',{19 'labels' 'Pz'});
+EEG = pop_chanedit(EEG, 'lookup','C:\\eeglab2021.1\\plugins\\dipfit4.3\\standard_BEM\\elec\\standard_1005.elc', ...
+                   'changefield',{1 'labels' 'FP1'},'changefield',{2 'labels' 'FP2'},'changefield',{3 'labels' 'F3'}, ...
+                   'changefield',{4 'labels' 'F4'},'changefield',{5 'labels' 'C3'},'changefield',{6 'labels' 'C4'}, ...
+                   'changefield',{7 'labels' 'P3'},'changefield',{8 'labels' 'P4'},'changefield',{9 'labels' 'O1'}, ...
+                   'changefield',{10 'labels' 'O2'},'changefield',{11 'labels' 'F7'},'changefield',{12 'labels' 'F8'}, ...
+                   'changefield',{13 'labels' 'T3'},'changefield',{14 'labels' 'T4'},'changefield',{15 'labels' 'T5'}, ...
+                   'changefield',{16 'labels' 'T6'},'changefield',{17 'labels' 'Fz'},'changefield',{18 'labels' 'Cz'}, ...
+                   'changefield',{19 'labels' 'Pz'});
 EEG = eeg_checkset( EEG );
 EEG = pop_chanedit(EEG, 'lookup','C:\\eeglab2021.1\\plugins\\dipfit4.3\\standard_BEM\\elec\\standard_1005.elc');
 EEG = eeg_checkset( EEG );
 
 originalEEG = EEG;
 
-%{
-% remove DC offset
-removeDC = true;
-freqHP = 0.5;
-freqLP = 10;
-sRate = EEG.srate();
-filtOrder = 3*fix(sRate/freqHP);
-
-if removeDC == true
-    EEG = pop_eegfiltnew( EEG, 'locutoff',  freqHP, 'hicutoff', freqLP, 'filtorder', filtOrder);
-end
-%}
-
-cleanEEG = clean_rawdata(EEG, -1, [0.25 0.75], -1, -1, 5, 0.25);
- cleaned1 = vis_artifacts(cleanEEG,EEG);
- saveas(gcf, 'figures\firstPassArtifacting.png', 'png');
-    
 % set average reference
 EEG.nbchan = EEG.nbchan+1;
 EEG.data(end+1,:) = zeros(1, EEG.pnts);
@@ -47,7 +38,36 @@ EEG.chanlocs(1,EEG.nbchan).labels = 'initialReference';
 EEG = pop_reref(EEG, []);
 EEG = pop_select( EEG,'nochannel',{'initialReference'});
 
-% rawHPAveRef = pop_eegplot(EEG);
+% clean data
+% remove artifacts
+EEG = clean_rawdata(EEG, 'off', [0.25 0.75], 'off', 'off', 5, 1);
+vis_artifacts(EEG, originalEEG);
+saveas(gcf, 'figures\first_pass_artifacting.png', 'png');
 
-% run ICA
-EEG = pop_runica(EEG);
+%{
+% remove line noise
+signal = struct('data', EEG.data, 'srate', EEG.srate);
+lineNoiseIn = struct('lineNoiseMethod', 'clean', 'lineNoiseChannels', 1:EEG.nbchan, 'Fs', EEG.srate, ...
+                     'lineFrequencies', [60 120 180 240], 'p', 0.01, 'fScanBandWidth', 2, 'taperBandWidth', 2, ...
+                     'taperWindowSize', 4, 'taperWindowStep', 1, 'tau', 100, 'pad', 2, 'fPassBand', [0 EEG.srate/2], ...
+                     'maximumIterations', 10);
+                 
+[clnOutput, lineNoiseOut] = cleanLineNoise(signal, lineNoiseIn);
+EEG.data = clnOutput.data;
+    
+    
+vis_artifacts(EEG, EEG2);
+saveas(gcf, 'figures\remove_linenoise.png', 'png');
+%}
+
+% run AMICA
+numprocs = 1;
+max_threads = 1;
+num_models = 1;
+max_iter = 100;
+
+outdir = [ filePath, 'amicaouttmp' filesep ];
+
+[weights,sphere,mods] = runamica15(EEG.data, 'num_models',num_models, 'outdir',outdir, 'numprocs', numprocs, ...
+                                   'max_threads', max_threads, 'max_iter',max_iter);
+
